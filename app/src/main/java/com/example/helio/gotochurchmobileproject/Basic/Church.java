193596a6f550e99d169e70cb6b74dfc9aec597ae
heliofsjr18 <heliofsjr18@hotmail.com>
2017-11-04 20:43:02 -0300

    LoginScreen LandScape/Portrait and others Activities...

---
+++ app/src/main/java/com/example/helio/gotochurchmobileproject/Basic/Church.java
@@ -0,0 +1,8 @@
+package com.example.helio.gotochurchmobileproject.Basic;
+
+/**
+ * Created by helio on 04/11/2017.
+ */
+
+public class Church {
+}
--- app/src/main/java/com/example/helio/gotochurchmobileproject/GTC_NewChurchActivity.java
+++ app/src/main/java/com/example/helio/gotochurchmobileproject/GTC_NewChurchActivity.java
@@ -1,18 +1,13 @@
 package com.example.helio.gotochurchmobileproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GTC_NewChurchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,GTC_WelcomeActivity.class);
-        startActivity(intent);
-
-
+        setContentView(R.layout.activity_gtc__new_church);
     }
 }

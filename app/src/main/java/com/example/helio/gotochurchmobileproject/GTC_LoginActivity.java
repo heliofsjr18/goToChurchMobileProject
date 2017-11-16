b9b9dd7cb72e0f717f289d9228d8718b2a4c7807
dayvson <dayvsondw@hotmail.com>
2017-11-16 14:40:00 -0300

    Revert "Login implementado, consumindo dados do webservice; alterações na tela Welcome; criação de pacote de class "Util" adicionado a classe webservice.java."
    This reverts commit 619160318dc24e418139a018776b5a799ce544e0.

--- app/src/main/java/com/example/helio/gotochurchmobileproject/GTC_LoginActivity.java
+++ app/src/main/java/com/example/helio/gotochurchmobileproject/GTC_LoginActivity.java
@@ -1,22 +1,14 @@
 package com.example.helio.gotochurchmobileproject;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.StrictMode;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.example.helio.gotochurchmobileproject.Util.WebService;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class GTC_LoginActivity extends AppCompatActivity {
-    WebService ws;
-    String URL = "http://dayvsonnascimento.pythonanywhere.com/gotochurch/usuario/login";
-    String dadosUsuario = "";
+
+
     private View.OnClickListener onClickListenerSignUp = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -30,63 +22,6 @@ public class GTC_LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gtc__login);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
         findViewById(R.id.button_signUp).setOnClickListener(onClickListenerSignUp);
-    }
-
-    private boolean verificaLogin(){
-
-        EditText editTextEmail = (EditText) findViewById(R.id.editText);//Referência ao editText de Email
-        EditText editTextSenha = (EditText) findViewById(R.id.editText2);//Referência ao editText de Senha
-
-        String email = String.valueOf(editTextEmail.getText());//pega valor do editText de email
-        String senha = String.valueOf(editTextSenha.getText());//pega valor do editTest de senha
-        this.URL = "http://dayvsonnascimento.pythonanywhere.com/gotochurch/usuario/login";
-        this.URL += "?email="+email+"&senha="+senha;
-        //Toast.makeText(this, this.URL, Toast.LENGTH_SHORT).show();
-        ws = new WebService();
-        boolean retorno = false;
-        try{
-
-            String resultado = ws.getUrlContents(this.URL);  //Chama função que consome o web service
-
-            JSONArray resultJson = new JSONArray(resultado);// coverte a string de resultado em um array Json
-            JSONObject result;
-            int numResultado = 0;
-
-            for (int i = 0; i < resultJson.length(); i++) {
-                result = new JSONObject(resultJson.getString(i));
-
-                numResultado = Integer.parseInt(result.getString("resultado"));
-
-            }
-
-            if(numResultado == 0){
-                retorno = false;
-            }else if(numResultado == 1){
-                retorno = true;
-                this.dadosUsuario = resultado;
-            }
-
-        }catch (Exception  e){
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
-        return retorno;
-    }
-
-    void entrar(View v){
-        //Toast.makeText(this, this.URL, Toast.LENGTH_SHORT).show();
-        if(this.verificaLogin()){
-            Bundle bundle = new Bundle();
-            bundle.putString("dadosUsuario", this.dadosUsuario);
-            Intent it = new Intent(this, GTC_WelcomeActivity.class);
-            it.putExtras(bundle);
-            startActivity(it);
-        }else{
-            Toast.makeText(this, "Login Inválido!", Toast.LENGTH_SHORT).show();
-        }
     }
 }
--- app/src/main/java/com/example/helio/gotochurchmobileproject/GTC_WelcomeActivity.java
+++ app/src/main/java/com/example/helio/gotochurchmobileproject/GTC_WelcomeActivity.java
@@ -1,8 +1,9 @@
 package com.example.helio.gotochurchmobileproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,23 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class GTC_WelcomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    String dadosUsuario = "";
-    private View.OnClickListener onClickListenerMap = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intentMap = new Intent(GTC_WelcomeActivity.this, GTC_MapsActivity.class);
-            startActivity(intentMap);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +24,13 @@ public class GTC_WelcomeActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        /*fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -53,39 +40,6 @@ public class GTC_WelcomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        //findViewById(R.id.fab).setOnClickListener(onClickListenerMap);
-        fab.setOnClickListener(this.onClickListenerMap);
-        View navHeaderView = navigationView.getHeaderView(0);
-        try{
-
-            Intent it = getIntent();
-            Bundle bundle = it.getExtras();
-            String dadosUsuariou = bundle.getString("dadosUsuario");
-            this.dadosUsuario = dadosUsuariou;
-
-            JSONArray resultJson = new JSONArray(dadosUsuariou);
-            JSONObject result;
-
-            for (int i = 0; i < resultJson.length(); i++) {
-                result = new JSONObject(resultJson.getString(i));
-
-                JSONArray dadosJson = new JSONArray(result.getString("dados"));
-                JSONObject dados;
-
-                for(int x = 0; x < dadosJson.length(); x++){
-                    dados = new JSONObject(dadosJson.getString(x));//pega dados do usuario
-
-                    TextView txvUserName = (TextView) navHeaderView.findViewById(R.id.userName);
-                    TextView txvUserEmail = (TextView) navHeaderView.findViewById(R.id.userEmail);
-                    
-                    txvUserName.setText(dados.getString("first_name"));
-                    txvUserEmail.setText(dados.getString("email"));
-                }
-            }
-        }catch (Exception e){
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-        }
     }
 
     @Override
@@ -111,6 +65,7 @@ public class GTC_WelcomeActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -124,11 +79,12 @@ public class GTC_WelcomeActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        if (id == R.id.nav_setor) {
-            // Handle the camera action
-        } else if (id == R.id.nav_area) {
 
-        } else if (id == R.id.nav_congregacao) {
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 

42beee812ae93baf813aef80eb826a418b553f15
Jorge Marçal <jrgmdf@gmail.com>
2017-12-02 17:26:35 -0300

    Commit

--- app/src/main/java/com/example/helio/gotochurchmobileproject/ChurchFragment.java
+++ app/src/main/java/com/example/helio/gotochurchmobileproject/ChurchFragment.java
@@ -16,88 +16,37 @@ import com.example.helio.gotochurchmobileproject.Fachada.Fachada;
 
 import java.util.List;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ChurchFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ChurchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChurchFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    private ListView listaServicos;
+    private ListView listaChurchs;
     List<Church> churchs;
+    private Church mChurch;
+    private static final String EXTRA_CHURCH = "church";
+    private ListView lvChurch;
 
     public ChurchFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChurchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChurchFragment newInstance(String param1, String param2) {
-        ChurchFragment fragment = new ChurchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ChurchFragment newInstance(Church church) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(EXTRA_CHURCH, church);
+        ChurchFragment dialog = new ChurchFragment();
+        dialog.setArguments(bundle);
+        return dialog;
     }
-    public static ChurchFragment newInstance(String text) {
-        ChurchFragment f = new ChurchFragment();
-        Bundle b = new Bundle();
-        b.putString("msg", text);
-        f.setArguments(b);
 
-        return f;
-    }
-    public void carregarLista() {
-        //fachada = Fachada.getInstance(getActivity());
 
-        //listaServicos = (ListView) getActivity().findViewById(R.id.lvSericosAbertos);
-        /*List<Servico> servicos = null;
-        servicos = fachada.ListarServicosUsuario(fachada.usuarioLogado());
-        ListaAdapterServico adapterServico = new ListaAdapterServico(getActivity(), (ArrayList<Servico>) servicos);
-        listaServicos.setAdapter(adapterServico);*/
-
-        GTC_ChurchListActivity obj =  new GTC_ChurchListActivity();
-        obj.listChurch();
-    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mChurch = (Church) getArguments().getSerializable(EXTRA_CHURCH);
         }
     }
 
-    /*@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_church, container, false);
-    }*/
-
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -136,7 +85,7 @@ public class ChurchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_church, container, false);
-        listaServicos = (ListView) view.findViewById(R.id.lvChurchFragment);
+        listaChurchs = (ListView) view.findViewById(R.id.lvChurchFragment);
         listaServicos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View v, int pos, long id) {
@@ -147,5 +96,36 @@ public class ChurchFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(
+                R.layout.fragment_church, container, false);
+        listaChurchs = (ListView) layout.findViewById(R.id.lvChurchFragment);
+        listaChurchs.requestFocus();
+
+        if (mChurch != null) {
+            listaChurchs.setText(mChurch.);
+        }
+        // Exibe o teclado virtual ao exibir o Dialog
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        getDialog().setTitle(R.string.acao_novo);
+        return layout;
+    }
+
+    public void carregarLista() {
+        //fachada = Fachada.getInstance(getActivity());
+
+        //listaServicos = (ListView) getActivity().findViewById(R.id.lvSericosAbertos);
+        /*List<Servico> servicos = null;
+        servicos = fachada.ListarServicosUsuario(fachada.usuarioLogado());
+        ListaAdapterServico adapterServico = new ListaAdapterServico(getActivity(), (ArrayList<Servico>) servicos);
+        listaServicos.setAdapter(adapterServico);*/
+
+        GTC_ChurchListActivity obj =  new GTC_ChurchListActivity();
+        obj.listChurch();
     }
 }
